e91a8b79c779d24975700470a107aa26eff66fe2
bertrandamobi <bertrandamobi768@gmail.com>
2023-08-10 18:35:00 +0100

    Demo data seed

---
+++ frontend/src/data/demoSeed.js
@@ -0,0 +1,14 @@
+export const assets = ['BTCUSDT', 'ETHUSDT', 'SOLUSDT', 'AAPL', 'NVDA'];
+export const initialPrices = {
+  BTCUSDT: 68240,
+  ETHUSDT: 3275,
+  SOLUSDT: 176,
+  AAPL: 205,
+  NVDA: 971,
+};
+
+export const initialPortfolio = [
+  { symbol: 'BTCUSDT', qty: 0.61, avg: 64120 },
+  { symbol: 'ETHUSDT', qty: 4.8, avg: 2870 },
+  { symbol: 'AAPL', qty: 72, avg: 187 },
+];
